18783d4ab21f25b970109289e47c09866cbcaa28
Andrew Besedin <andrewprog97@gmail.com>
2025-09-25 15:07:30 +0300

    add files upload

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Router } from "express";
 import path from 'path';
 import { fileURLToPath } from "url";
 import { JsonDB, Config } from 'node-json-db';
+import multer from "multer";
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +10,18 @@ const PORT = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, "./uploads"));
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/", express.static(path.join(__dirname, "static")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -78,13 +91,33 @@ router.patch("/task/:id/status", async (req, res) => {
 
   const task = tasks.find(t => t.id === taskId);
 
-  if (task) {
-    task.status = req.body.newStatus;
-    await db.push("/tasks", tasks);
-    res.status(200).send("");
-  } else {
+  if (!task) {
     res.status(404).send("");
+    return;
   }
+
+  task.status = req.body.newStatus;
+  await db.push("/tasks", tasks);
+  res.status(200).send("");
+});
+
+router.post("/task/:id/uploaded-files", upload.array("attached-files"), async (req, res) => {
+  const taskId = req.params.id;
+  const tasksObjs = await db.getObjectDefault("/tasks", []);
+
+  const tasks = tasksObjs.map(task => new Task(task));
+
+  const task = tasks.find(t => t.id === taskId);
+
+  if (!task) {
+    res.status(404).send("");
+    return; 
+  }
+
+  const uploadedFiles = (req.files as Express.Multer.File[]).map(file => file.filename);
+  task.fileNames = [...task.fileNames, ...uploadedFiles];
+  await db.push("/tasks", tasks);
+  res.status(200).send("");
 });
 
 app.use("/api", router);
